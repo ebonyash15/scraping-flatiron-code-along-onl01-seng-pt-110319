e8155d1e1a634980fbb3d00651d4a5e8e620088f
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-20 04:07:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
-
 require_relative './course.rb'
 
 class Scraper
-  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +13,18 @@ class Scraper
       end
     end
   end
-  
+  def get_page
+    page_html=open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    @doc=Nokogiri::HTML(page_html)
+    p @doc
+  end
+  def get_courses
+    self.get_page
+    @courses=@doc.css("#course-grid.block.posts-holder")
+    #id="#course-grid" class=".block", class=".posts-holder",
+      #class=".post same-height-left/same-height-right", <h2>Web Development Immersive</h2>
+      p @courses
+  end
+  def make_courses
+  end
 end
-
-
-
